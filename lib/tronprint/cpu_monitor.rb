c3130d7597d53c9f42bd3cd59062acc447254a36
Derek Kastner <dkastner@gmail.com>
2010-12-28 16:19:30 -0500

    Finished CPUMonitor

--- lib/tronprint/cpu_monitor.rb
+++ lib/tronprint/cpu_monitor.rb
@@ -1,9 +1,16 @@
 module Tronprint
   class CPUMonitor < Thread
+    attr_accessor :total_recorded_cpu_time
+
     def initialize(options = {})
       options[:run] ||= true
       if options[:run]
-        super() {}
+        super do
+          while(true) do
+           monitor
+           sleep(5)
+          end
+        end
       end
     end
 
@@ -12,8 +19,21 @@ module Tronprint
     end
 
     def monitor
-      Aggregator.update key, elapsed_cpu_time
-      sleep 5  # take five, little thread
+      elapsed_time = elapsed_cpu_time
+      Aggregator.update key, elapsed_time
+      self.total_recorded_cpu_time = elapsed_time
+    end
+
+    def total_cpu_time
+      Process.times.inject(0) { |sum, i| sum += i }
+    end
+
+    def total_recorded_cpu_time
+      @total_recorded_cpu_time ||= 0
+    end
+
+    def elapsed_cpu_time
+      total_cpu_time - total_recorded_cpu_time
     end
   end
 end
--- spec/tronprint/cpu_monitor_spec.rb
+++ spec/tronprint/cpu_monitor_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 
 describe Tronprint::CPUMonitor do
   let(:cpu_monitor) { Tronprint::CPUMonitor.new :run => false }
-  describe 'monitor' do
+  describe '#monitor' do
     before :each do
-      cpu_monitor.stub! :sleep
       cpu_monitor.stub!(:elapsed_cpu_time).and_return 23.87
       Tronprint::Aggregator.stub!(:update)
     end
@@ -14,19 +13,44 @@ describe Tronprint::CPUMonitor do
         with('my_app/application/cpu_time', 23.87)
       cpu_monitor.monitor
     end
-    it 'should sleep when it is done' do
-      cpu_monitor.should_receive :sleep
+    it 'should increment the toal recorded cpu time' do
+      cpu_monitor.stub!(:elapsed_cpu_time).and_return 9.0
       cpu_monitor.monitor
+      cpu_monitor.total_recorded_cpu_time.should == 9.0
+      cpu_monitor.stub!(:elapsed_cpu_time).and_return 12.0
+      cpu_monitor.monitor
+      cpu_monitor.total_recorded_cpu_time.should == 12.0
     end
   end
 
-  describe 'elapsed_cpu_time' do
-    it 'should return the total CPU time on the first run'
-    it 'should return the amount of CPU time used since the last check'
+  describe '#elapsed_cpu_time' do
+    it 'should return the total CPU time on the first run' do
+      cpu_monitor.stub!(:total_recorded_cpu_time).and_return 0
+      cpu_monitor.stub!(:total_cpu_time).and_return 9.0
+      cpu_monitor.elapsed_cpu_time.should == 9.0
+    end
+    it 'should return the amount of CPU time used since the last check' do
+      cpu_monitor.stub!(:total_recorded_cpu_time).and_return 23.0
+      cpu_monitor.stub!(:total_cpu_time).and_return 36.0
+      cpu_monitor.elapsed_cpu_time.should == 13.0
+    end
   end
 
-  describe 'record_time' do
-    it 'should update the total CPU time for the current application'
+  describe '#total_cpu_time' do
+    it 'should return the total user and system time of the process' do
+      Process.stub!(:times).and_return [10.1, 12.3]
+      cpu_monitor.total_cpu_time.should == 22.4
+    end
+  end
+
+  describe '#total_recorded_cpu_time' do
+    it 'should return 0 by default' do
+      cpu_monitor.total_recorded_cpu_time.should == 0
+    end
+    it 'should return total recorded time' do
+      cpu_monitor.total_recorded_cpu_time = 3
+      cpu_monitor.total_recorded_cpu_time.should == 3
+    end
   end
 end
 
