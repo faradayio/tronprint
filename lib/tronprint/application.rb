41add7df170bacf51d9d69277e8f97188f31123d
Derek Kastner <dkastner@gmail.com>
2011-01-07 12:08:37 -0500

    Application doesn't worry about name anymore

--- lib/tronprint/application.rb
+++ lib/tronprint/application.rb
@@ -5,20 +5,16 @@ module Tronprint
     include Carbon
 
     emit_as :computation do
-      provide :duration      # hours
+      provide :duration    # hours
       provide :zip_code
     end
 
-    attr_accessor :duration, :zip_code, :name
+    attr_accessor :duration, :zip_code
 
     def initialize(attrs = {})
       attrs.each do |name, value|
         self.send("#{name}=", value)
       end
-    end
-
-    def name
-      @name ||= File.basename(Dir.pwd)
     end
   end
 end
---
+++ spec/tronprint/application_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Tronprint::Application do
+  let(:application) { Tronprint::Application.new }
+
+  describe '#duration' do
+
+  end
+end
+
