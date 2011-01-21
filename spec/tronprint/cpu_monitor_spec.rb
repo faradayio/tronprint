70eda576276c715820695369befaa4bf340dba94
Derek Kastner <dkastner@gmail.com>
2011-01-21 16:30:51 -0500

    Revert "Tronprint wasn't loading total CPU times from past runs"
    This reverts commit 3676b6a9045198131fdf7a80410bf78b9cc5a4b1.

--- spec/tronprint/cpu_monitor_spec.rb
+++ spec/tronprint/cpu_monitor_spec.rb
@@ -13,8 +13,6 @@ describe Tronprint::CPUMonitor do
       aggregator['my_app/application/cpu_time'].should == 23.87
     end
     it 'should increment the toal recorded cpu time' do
-      aggregator[cpu_monitor.key] = nil
-      cpu_monitor.instance_variable_set :@total_recorded_cpu_time, nil
       cpu_monitor.stub!(:elapsed_cpu_time).and_return 9.0
       cpu_monitor.monitor
       cpu_monitor.total_recorded_cpu_time.should == 9.0
@@ -35,12 +33,6 @@ describe Tronprint::CPUMonitor do
       cpu_monitor.stub!(:total_cpu_time).and_return 36.0
       cpu_monitor.elapsed_cpu_time.should == 13.0
     end
-    it 'should load recorded cpu time from previous runs when process is restarted' do
-      cpu_monitor.instance_variable_set :@total_recorded_cpu_time, nil
-      aggregator[cpu_monitor.key] = 12.5
-      cpu_monitor.stub!(:total_cpu_time).and_return 36.0
-      cpu_monitor.elapsed_cpu_time.should == 23.5
-    end
   end
 
   describe '#total_cpu_time' do
@@ -51,16 +43,12 @@ describe Tronprint::CPUMonitor do
   end
 
   describe '#total_recorded_cpu_time' do
-    it 'should return nil by default' do
-      cpu_monitor.total_recorded_cpu_time.should be_nil
+    it 'should return 0 by default' do
+      cpu_monitor.total_recorded_cpu_time.should == 0
     end
     it 'should return total recorded time' do
       cpu_monitor.total_recorded_cpu_time = 3
       cpu_monitor.total_recorded_cpu_time.should == 3
-    end
-    it 'should load previously recorded times from previous runs' do
-      aggregator[cpu_monitor.key] = 12.4
-      cpu_monitor.total_recorded_cpu_time.should == 12.4
     end
   end
 end
