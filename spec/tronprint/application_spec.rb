d2722641a4011f08854ce70553c3c8f194bde024
Derek Kastner <dkastner@gmail.com>
2011-06-13 13:47:25 -0400

    Wait a minute, this one goes here, that one goes there!

--- spec/tronprint/application_spec.rb
+++ spec/tronprint/application_spec.rb
@@ -1,10 +1,9 @@
 require 'spec_helper'
 
 describe Tronprint::Application do
-  let(:application) { Tronprint::Application.new }
-
-  describe '#duration' do
-
+  it 'reports emission esimates for application-related activity' do
+    app = Tronprint::Application.new :duration => 72831, :zip_code => 48915
+    app.should respond_to(:emission_estimate)
   end
 end
 
--- spec/tronprint/computer_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Tronprint::Application do
-  it 'reports emission esimates for application-related activity' do
-    app = Tronprint::Application.new :duration => 72831, :zip_code => 48915
-    app.should respond_to(:emission_estimate)
-  end
-end
-
