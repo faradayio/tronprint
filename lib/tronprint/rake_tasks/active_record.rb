3e188fc61bd65556e088ab19aed56ed2587feae9
Derek Kastner <dkastner@gmail.com>
2011-06-15 14:56:01 -0400

    Changed purpose of rake task

--- lib/tronprint/rake_tasks/active_record.rb
+++ lib/tronprint/rake_tasks/active_record.rb
@@ -1,5 +1,5 @@
 namespace :tronprint do
-  desc 'Set up ActiveRecord datastore for Tronprint'
+  desc 'Manually set up ActiveRecord datastore for Tronprint'
   task :moneta => :environment do
     adapter_options = Tronprint.aggregator_options
     ar = Moneta::Adapters::ActiveRecord.new
@@ -7,5 +7,3 @@ namespace :tronprint do
     puts 'Ensured that Moneta::Adapters::ActiveRecord::Store exists'
   end
 end
-
-task 'db:migrate' => 'tronprint:moneta'
